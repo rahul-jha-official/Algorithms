f310e8db34b204036f000105ccca5926eecd582e
Rahul Jha <138975150+rahul-jha-official@users.noreply.github.com>
2024-02-18 22:03:21 +0530

    Update BinarySearch.cs

--- Searching/BinarySearch.cs
+++ Searching/BinarySearch.cs
@@ -1,27 +1,29 @@
 public class BinarySearch
 {
-    public int Search(int[] list, int element)
+    public int Search<T>(T[] list, T element) where T : IComparable<T>
     {
         int start = 0, end = list.Length - 1;
         while (start <= end)
         {
             int middle = (end - start) / 2 + start;
-            if (list[middle] == element) return middle;
-            else if (list[middle] < element) start = middle + 1;
+            int status = element.CompareTo(list[middle]);
+            if (status == 0) return middle;
+            else if (status > 0) start = middle + 1;
             else end = middle - 1;
         }
         return -1;
     }
 
     //Recursive Search
-    public int Search(int[] list, int element, int start, int end)
+    public int Search<T>(T[] list, T element, int start, int end) where T : IComparable<T>
     {
         if (start > end) return -1;
 
         int middle = (end - start) / 2 + start;
+        int status = element.CompareTo(list[middle]);
 
-        if (list[middle] == element) return middle;
-        else if (list[middle] < element) return Search(list, element, middle + 1, end);
+        if (status == 0) return middle;
+        else if (status > 0) return Search(list, element, middle + 1, end);
         else return Search(list, element, start, middle - 1);
     }
 }
